38c98e0942fbf540a7c7a1b58a8c4c26517c877a
Keefer Taylor <keefer@ripple.com>
2019-07-31 12:54:39 -0700

    alphabetization

--- terram.js
+++ terram.js
@@ -1,12 +1,12 @@
-const utils = require('./utils.js');
 const Wallet = require('./wallet.js');
+const utils = require('./utils.js');
 
 /**
  * Terram provides a javascript foundation for XRP Wallet and Exchange APIs.
  */
 const terram = {
-  utils: utils,
-  Wallet: Wallet
+  Wallet: Wallet,
+  utils: utils
 }
 
 module.exports = Terram;
