b7eb2e6665952a50e7fb642590ba29c1abac6918
Keefer Taylor <keefer@ripple.com>
2019-08-06 16:45:58 -0400

    cleaning up tests

--- serializer.js
+++ serializer.js
@@ -5,16 +5,25 @@ const { Transaction } = require('./generated/Transaction_pb.js');
  * Provides functionality to serialize from protocol buffers to JSON objects.
  */
 class Serializer {
+    /**
+     * Convert a transaction to a JSON representation.
+     * 
+     * @param {proto.Transaction} transaction A transaction protocol buffer to convert.
+     * @returns {Object} The JSON representation of the given transaction.
+     */
     static serializeTransaction(transaction) {
+        // Serialize the protocol buffer to a JSON representation.
         var object = transaction.toObject();
 
+        // Convert fields to upper case.
         this.convertPropertyName("account", "Account", object);
         this.convertPropertyName("sequence", "Sequence", object);
 
+        // Convert XRP denominated fee field.
         object.Fee = this.xrpAmountToJSON(transaction.getFee());
         delete object.fee;
 
-
+        // Convert additional transaction data.
         const transactionDataCase = transaction.getTransactionDataCase();
         switch (transactionDataCase) {
             case Transaction.TransactionDataCase.PAYMENT:
@@ -25,7 +34,11 @@ class Serializer {
         return object;
     }
 
-
+    /**
+     * 
+     * @param {proto.Payment} paymentData 
+     * @param {*} object 
+     */
     static convertPaymentData(paymentData, object) {
         object.TransactionType = "Payment";
         object.Destination = paymentData.getDestination();
@@ -33,32 +46,60 @@ class Serializer {
         const amountCase = paymentData.getAmountCase();
         switch (amountCase) {
             case Payment.AmountCase.FIAT_AMOUNT:
-                // TODO: Implement me.
+                object.Amount = this.fiatAmountToJSON(paymentData.getFiatAmount());
                 break;
             case Payment.AmountCase.XRP_AMOUNT:
                 object.Amount = this.xrpAmountToJSON(paymentData.getXrpAmount());
                 break;
         }
-
-
-
         delete object.payment
-
-
-
-        // object.Amount = paymentData.getAmount();
-    }
-    static getAmountField(proto) {
-        const amountCase = proto.getAmountCase();
-
-
     }
 
+    /**
+     * Change the name of a field in an object while preserving the value.
+     * 
+     * @note This method has side effects to the `object` parameter.
+     * 
+     * @param {String} oldPropertyName The property name to convert from.
+     * @param {String} newPropertyName The new property name.
+     * @param {Object} object The object on which the conversion is performed.
+     */
     static convertPropertyName(oldPropertyName, newPropertyName, object) {
         object[newPropertyName] = object[oldPropertyName];
         delete object[oldPropertyName];        
     }
 
+    /**
+     * Convert a FiatAmount proto to a JSON representation.
+     * 
+     * @param {proto.FiatAmount} fiatAmount The FiatAmount to convert.
+     * @returns {Object} The FiatAmount as JSON.
+     */
+    static fiatAmountToJSON(fiatAmount) {
+        const json = fiatAmount.toObject();
+        json.currency = this.currencyValueToJSON(fiatAmount.getCurrency());
+        return json;
+    }
+
+    /**
+     * Convert a Currency enum to a JSON representation.
+     *  
+     * @param {proto.FiatAmount.Currency} currency The Currency to convert.
+     * @returns {String} The Currency as JSON.
+     */
+    static currencyValueToJSON(currency) {
+        switch (currency) {
+            case proto.FiatAmount.Currency.USD:
+                return "USD";
+        }
+    }
+
+    /**
+     * Convert an XRPAmount proto to a JSON representation.
+     * 
+     * @param {proto.XRPAmount} xrpAmount The XRPAmount to convert.
+     * @return {String} The XRPAmount as JSON.
+     */
     static xrpAmountToJSON(xrpAmount) {
         return xrpAmount.getDrops() + "";
     }
--- test/serializer-test.js
+++ test/serializer-test.js
@@ -6,49 +6,89 @@ const { XRPAmount } = require('../generated/XRPAmount_pb.js');
 const { assert } = require('chai')
 
 
-const tx_json = {
-    Account: 'r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ',
-    Amount: '1000',
-    Destination: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
-    Fee: '10',
-    Sequence: 1,
-    TransactionType: 'Payment',
-};
-
 describe('serializer', () => {
-    it('serializes', () => {
+    it('serializes payment in XRP', () => {
+        // GIVEN a transaction which represents a payment denominated in Fiat.
+        const value = 1000;
+        const destination = "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+        const fee = 10;
+        const sequence = 1;
+        const account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+
         const paymentAmount = new XRPAmount();
-        paymentAmount.setDrops(1000);
+        paymentAmount.setDrops(value);
 
         const payment = new Payment();
         payment.setDestination("rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh");
         payment.setXrpAmount(paymentAmount);
 
-        const fee = new XRPAmount();
-        fee.setDrops(10);
+        const transactionFee = new XRPAmount();
+        transactionFee.setDrops(10);
 
         const transaction = new Transaction();
         transaction.setAccount("r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ");
-        transaction.setFee(fee);
+        transaction.setFee(transactionFee);
         transaction.setSequence(1);
         transaction.setPayment(payment);
         
+        // WHEN the transaction is serialized to JSON.
         const serialized = Serializer.serializeTransaction(transaction);
 
-
-        console.log(JSON.stringify(serialized));
-
-        assert.deepEqual(serialized, tx_json);
-
-
+        // THEN the result is as expected.
+        const expectedJSON = {
+            Account: account,
+            Amount: value + "",
+            Destination: destination,
+            Fee: fee + "",
+            Sequence: sequence,
+            TransactionType: 'Payment'
+        };
+        assert.deepEqual(serialized, expectedJSON);
     });
 
-    // it('serializes XRP Amount', () => {
-    //     const drops = "100"; 
-    //     const amount = new XRPAmount();
-    //     amount.setDrops(drops);
-    //     const serialized = Serializer.serializeXRPAmount(amount);
-    //     assert.equal(serialized, drops);
-    // });
+    it('serializes payment in Fiat', () => {
+        // GIVEN a transaction which represents a payment denominated in Fiat.
+        const issuer = "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59";
+        const value = "153.75";
+        const destination = "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+        const fee = 10;
+        const sequence = 1;
+        const account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
 
+        const paymentAmount = new FiatAmount();
+        paymentAmount.setIssuer(issuer);
+        paymentAmount.setValue(value);
+        paymentAmount.setCurrency(FiatAmount.Currency.USD);
+
+        const payment = new Payment();
+        payment.setDestination(destination);
+        payment.setFiatAmount(paymentAmount);
+
+        const transactionFee = new XRPAmount();
+        transactionFee.setDrops(fee);
+
+        const transaction = new Transaction();
+        transaction.setAccount(account);
+        transaction.setFee(transactionFee);
+        transaction.setSequence(sequence);
+        transaction.setPayment(payment);
+        
+        // WHEN the transaction is serialized to JSON.
+        const serialized = Serializer.serializeTransaction(transaction);
+
+        // THEN the result is as expected.
+        const expectedJSON = {
+            Account: account,
+            Amount: {
+                "currency": "USD",
+                "value": value,
+                "issuer": issuer
+            },
+            Destination: destination,
+            Fee: fee + "",
+            Sequence: sequence,
+            TransactionType: 'Payment',
+        };
+        assert.deepEqual(serialized, expectedJSON);
+    });
 });
