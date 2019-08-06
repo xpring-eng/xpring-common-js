e28e2311e6948dd770f7760e3a0a0ea2eb6f9f42
Keefer Taylor <keefer@ripple.com>
2019-08-06 17:04:38 -0400

    Fix script

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -3,8 +3,11 @@
 set -e -o pipefail
 
 echo "Regenerating Terram Protos..."
-protoc --proto_path=./terram-protos/ \
+
+mkdir -p ./generated
+
+protoc --proto_path=../terram-protos/ \
 --js_out=import_style=commonjs,binary:./generated/ \
-./terram-protos/*.proto
+../terram-protos/*.proto
 
 echo "All done!"
