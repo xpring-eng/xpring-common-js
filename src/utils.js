48ffbfd69c21bdd042c2c1ab0c1fe8a9d425ecac
Keefer Taylor <keefer.t@gmail.com>
2019-08-09 11:46:24 -0400

    restore utils

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,11 @@
 "use strict";
+
 var addressCodec = require("ripple-address-codec");
+
 var utils = {
   isValidAddress: function(address) {
     return addressCodec.isValidAddress(address);
   }
 };
+
 module.exports = utils;
