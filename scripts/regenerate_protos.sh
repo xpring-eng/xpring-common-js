45cbab25ff242d2e9862f983c8f77335cbc1e038
Keefer Taylor <keefer.t@gmail.com>
2019-08-14 13:32:23 -0400

    Upgrade generation script

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -6,8 +6,8 @@ echo "Regenerating Terram Protos..."
 
 mkdir -p ./generated
 
-protoc --proto_path=$PWD/terram-protos/ \
+protoc --proto_path=$PWD/terram-protos/models \
 --js_out=import_style=commonjs,binary:$PWD/generated/ \
-$PWD/terram-protos/*.proto
+$PWD/terram-protos/models/*.proto
 
 echo "All done!"
