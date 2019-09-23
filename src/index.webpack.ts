3b7595c15ebbe5832906fda9104d6b45499fdcc6
Keefer Taylor <keefer.t@gmail.com>
2019-09-23 17:36:03 -0400

    Revert functions

--- src/index.webpack.ts
+++ src/index.webpack.ts
@@ -4,16 +4,6 @@ import { Transaction } from "../generated/transaction_pb";
 import Wallet from "./wallet";
 import Utils from "./utils";
 
-function fromHexToString() {
-  return new
-}
-const fromHexString = hexString =>
-  new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-
-const toHexString = bytes =>
-  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
-
-
 /**
  * Terram provides a JavaScript foundation for the Xpring Platform.
  */
