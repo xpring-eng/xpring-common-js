605fba3d9e047cb3b0452d70a100458e4c9bcc85
Keefer Taylor <keefer.t@gmail.com>
2019-09-27 14:25:36 -0400

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { AccountInfo } from '../generated/account_info_pb';
-import { Currency } from '../generated/currency_pb';
-import { Fee } from '../generated/fee_pb';
-import { FiatAmount } from '../generated/fiat_amount_pb';
-import { GetAccountInfoRequest } from '../generated/get_account_info_request_pb';
-import { GetFeeRequest } from '../generated/get_fee_request_pb';
-import { Payment } from '../generated/payment_pb';
-import { SubmitSignedTransactionRequest } from '../generated/submit_signed_transaction_request_pb';
-import { SubmitSignedTransactionResponse } from '../generated/submit_signed_transaction_response_pb';
-import { XRPAmount } from '../generated/xrp_amount_pb';
-import { XRPLedgerClient, XRPLedgerService } from '../generated/xrp_ledger_grpc_pb';
+import { AccountInfo } from "../generated/account_info_pb";
+import { Currency } from "../generated/currency_pb";
+import { Fee } from "../generated/fee_pb";
+import { FiatAmount } from "../generated/fiat_amount_pb";
+import { GetAccountInfoRequest } from "../generated/get_account_info_request_pb";
+import { GetFeeRequest } from "../generated/get_fee_request_pb";
+import { Payment } from "../generated/payment_pb";
+import { SubmitSignedTransactionRequest } from "../generated/submit_signed_transaction_request_pb";
+import { SubmitSignedTransactionResponse } from "../generated/submit_signed_transaction_response_pb";
+import { XRPAmount } from "../generated/xrp_amount_pb";
+import {
+  XRPLedgerClient,
+  XRPLedgerService
+} from "../generated/xrp_ledger_grpc_pb";
 
 import SignedTransaction from "../generated/signed_transaction_pb";
 import Signer from "../src/signer";
