d016f7c0f80a324022e595e645f921e67fe1e785
Keefer Taylor <keefer.t@gmail.com>
2019-09-27 15:51:55 -0400

    polish

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,27 @@ export { AccountInfo } from "../generated/account_info_pb";
 export { Currency } from "../generated/currency_pb";
 export { Fee } from "../generated/fee_pb";
 export { FiatAmount } from "../generated/fiat_amount_pb";
-export { GetAccountInfoRequest } from "../generated/get_account_info_request_pb";
+export {
+  GetAccountInfoRequest
+} from "../generated/get_account_info_request_pb";
 export { GetFeeRequest } from "../generated/get_fee_request_pb";
 export { Payment } from "../generated/payment_pb";
-export { SubmitSignedTransactionRequest } from "../generated/submit_signed_transaction_request_pb";
-export { SubmitSignedTransactionResponse } from "../generated/submit_signed_transaction_response_pb";
+export {
+  SubmitSignedTransactionRequest
+} from "../generated/submit_signed_transaction_request_pb";
+export {
+  SubmitSignedTransactionResponse
+} from "../generated/submit_signed_transaction_response_pb";
 export { XRPAmount } from "../generated/xrp_amount_pb";
 export {
   XRPLedgerClient,
   XRPLedgerService
 } from "../generated/xrp_ledger_grpc_pb";
-
 export { SignedTransaction } from "../generated/signed_transaction_pb";
-export { default as Signer} from "../src/signer";
-export { Transaction} from "../generated/transaction_pb";
+export { default as Signer } from "../src/signer";
+export { Transaction } from "../generated/transaction_pb";
 
-export { default as Wallet} from "./wallet";
+export { default as Wallet } from "./wallet";
 export { WalletGenerationResult } from "./wallet";
-export { default as Utils} from "./utils";
-export { default as grpc } from "grpc"
+export { default as Utils } from "./utils";
+export { default as grpc } from "grpc";
