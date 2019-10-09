d62501bf74fe580cc8ef6606a43c50ceb2445182
Keefer Taylor <keefer.t@gmail.com>
2019-10-09 15:59:17 -0400

    disable some linter rules on tests

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,17 @@
 
 var addressCodec = require("ripple-address-codec");
 
+/**
+ * A simple property bag which contains components of a classic address. Components contained in this object are neither sanitized or validated.
+ */
+export interface ClassicAddress {
+  /** A classic address. */
+  address: string;
+
+  /** An optional tag. */
+  tag: number | undefined;
+}
+
 class Utils {
   /**
    * Validate that the given string is a valid address on the XRP Ledger.
@@ -21,21 +32,47 @@ class Utils {
 
   /**
    * Encode the given classic address and tag into an x-address.
-   * 
+   *
    * @see https://xrpaddress.info/
-   * 
+   *
    * @param classicAddress A classic address to encode.
    * @param tag An optional tag to encode.
    * @returns A new x-address if inputs were valid, otherwise undefined.
    */
-  public static encodeXAddress(classicAddress: string, tag: number|undefined): string|undefined {
+  public static encodeXAddress(
+    classicAddress: string,
+    tag: number | undefined
+  ): string | undefined {
     if (!addressCodec.isValidClassicAddress(classicAddress)) {
       return undefined;
     }
 
     // Xpring Common JS's API takes a string|undefined while the underlying address library wants string|false.
     const shimTagParameter = tag != undefined ? tag : false;
-    return addressCodec.classicAddressToXAddress(classicAddress, shimTagParameter)
+    return addressCodec.classicAddressToXAddress(
+      classicAddress,
+      shimTagParameter
+    );
+  }
+
+  /**
+   * Decode a `ClassicAddress` from a given x-address.
+   *
+   * @see https://xrpaddress.info/
+   *
+   * @param xAddress The xAddress to decode.
+   * @returns A `ClassicAddress`
+   */
+  public static decodeXAddress(xAddress: string): ClassicAddress | undefined {
+    if (!addressCodec.isValidXAddress(xAddress)) {
+      return undefined;
+    }
+
+    let shimClassicAddress = addressCodec.xAddressToClassicAddress(xAddress);
+    return {
+      address: shimClassicAddress.classicAddress,
+      tag: shimClassicAddress.tag != false ? shimClassicAddress.tag : undefined
+    };
   }
 
   /**
