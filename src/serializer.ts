42edba397a7f70a9b3479478e522e569a35b3543
Keefer Taylor <keefer.t@gmail.com>
2019-10-18 11:49:16 -0500

    Run linter

--- src/serializer.ts
+++ src/serializer.ts
@@ -119,7 +119,7 @@ class Serializer {
    * @param {proto.FiatAmount.Currency} currency The Currency to convert.
    * @returns {String} The Currency as JSON.
    */
-  private static currencyToJSON(currency: Currency): string|undefined {
+  private static currencyToJSON(currency: Currency): string | undefined {
     switch (currency) {
       case Currency.CURRENCY_INVALID:
         return undefined;
