0a734f6249ce1d37869129d357feed3d96a8546a
Keefer Taylor <keefer.t@gmail.com>
2019-11-12 18:02:38 -0500

    add tx status rpc

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,16 @@ export { AccountInfo } from "../generated/account_info_pb";
 export { Currency } from "../generated/currency_pb";
 export { Fee } from "../generated/fee_pb";
 export { FiatAmount } from "../generated/fiat_amount_pb";
-export {
-  GetAccountInfoRequest
-} from "../generated/get_account_info_request_pb";
+export { GetAccountInfoRequest } from "../generated/get_account_info_request_pb";
 export { GetFeeRequest } from "../generated/get_fee_request_pb";
+export { GetTransactionStatusRequest } from "../generated/get_transaction_status_request_pb";
 export { Payment } from "../generated/payment_pb";
-export {
-  SubmitSignedTransactionRequest
-} from "../generated/submit_signed_transaction_request_pb";
-export {
-  SubmitSignedTransactionResponse
-} from "../generated/submit_signed_transaction_response_pb";
+export { SubmitSignedTransactionRequest } from "../generated/submit_signed_transaction_request_pb";
+export { SubmitSignedTransactionResponse } from "../generated/submit_signed_transaction_response_pb";
 export { XRPAmount } from "../generated/xrp_amount_pb";
 export { SignedTransaction } from "../generated/signed_transaction_pb";
 export { default as Signer } from "../src/signer";
+export { TransactionStatus } from "../generated/transaction_status_pb";
 export { Transaction } from "../generated/transaction_pb";
 
 export { default as Wallet } from "./wallet";
