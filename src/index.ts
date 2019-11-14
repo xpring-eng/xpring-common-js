b13c39940d0909e5607716df7e375293156accfc
Keefer Taylor <keefer.t@gmail.com>
2019-11-13 21:29:30 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,13 @@ export { Currency } from "../generated/currency_pb";
 export { Fee } from "../generated/fee_pb";
 export { FiatAmount } from "../generated/fiat_amount_pb";
 export { GetLatestValidatedLedgerSequenceRequest } from "../generated/get_latest_validated_ledger_sequence_request_pb";
-export {
-  GetAccountInfoRequest
-} from "../generated/get_account_info_request_pb";
+export { GetAccountInfoRequest } from "../generated/get_account_info_request_pb";
 export { GetFeeRequest } from "../generated/get_fee_request_pb";
-export {
-  GetTransactionStatusRequest
-} from "../generated/get_transaction_status_request_pb";
+export { GetTransactionStatusRequest } from "../generated/get_transaction_status_request_pb";
 export { LedgerSequence } from "../generated/ledger_sequence_pb";
 export { Payment } from "../generated/payment_pb";
-export {
-  SubmitSignedTransactionRequest
-} from "../generated/submit_signed_transaction_request_pb";
-export {
-  SubmitSignedTransactionResponse
-} from "../generated/submit_signed_transaction_response_pb";
+export { SubmitSignedTransactionRequest } from "../generated/submit_signed_transaction_request_pb";
+export { SubmitSignedTransactionResponse } from "../generated/submit_signed_transaction_response_pb";
 export { XRPAmount } from "../generated/xrp_amount_pb";
 export { SignedTransaction } from "../generated/signed_transaction_pb";
 export { default as Signer } from "../src/signer";
