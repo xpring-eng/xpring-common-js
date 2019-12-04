c655f35a54690f86d42eddda63224b4b39b514b8
Keefer Taylor <keefer.t@gmail.com>
2019-12-04 15:09:19 -0500

    Do not generate grpc stubs in xpring-common-js, have clients do this

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -11,22 +11,28 @@ TS_OUT_DIR="./generated"
 mkdir -p $TS_OUT_DIR
 mkdir -p $JS_OUT_DIR
 
-# Generate node code.
-grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:$TS_OUT_DIR \
-    --js_out=import_style=commonjs,binary:$JS_OUT_DIR \
-    --grpc_out=$TS_OUT_DIR \
-    --grpc_out=$JS_OUT_DIR \
-    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-    --proto_path=$PWD/xpring-common-protocol-buffers/proto \
+protoc --proto_path=$PWD/xpring-common-protocol-buffers/proto \
+    --js_out=import_style=commonjs:$JS_OUT_DIR \
+    --js_out=import_style=commonjs:$TS_OUT_DIR \
     $PWD/xpring-common-protocol-buffers/**/*.proto
 
-# Generate tyepscript declaration files.
-protoc \
-    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=$TS_OUT_DIR \
-    --ts_out=$JS_OUT_DIR \
-    --proto_path=$PWD/xpring-common-protocol-buffers/proto \
-    $PWD/xpring-common-protocol-buffers/**/*.proto
-
-echo "All done!"
+#
+# # Generate node code.
+# grpc_tools_node_protoc \
+#     --js_out=import_style=commonjs,binary:$TS_OUT_DIR \
+#     --js_out=import_style=commonjs,binary:$JS_OUT_DIR \
+#     --grpc_out=$TS_OUT_DIR \
+#     --grpc_out=$JS_OUT_DIR \
+#     --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+#     --proto_path=$PWD/xpring-common-protocol-buffers/proto \
+#     $PWD/xpring-common-protocol-buffers/**/*.proto
+#
+# # Generate tyepscript declaration files.
+# protoc \
+#     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+#     --ts_out=$TS_OUT_DIR \
+#     --ts_out=$JS_OUT_DIR \
+#     --proto_path=$PWD/xpring-common-protocol-buffers/proto \
+#     $PWD/xpring-common-protocol-buffers/**/*.proto
+#
+# echo "All done!"
