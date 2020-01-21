e038449ca04465cac7da0e5033f76b327b1ab3a1
Hans Bergren <hansbergren@gmail.com>
2020-01-21 12:16:39 -0500

    Single quotes in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
 
-  parser: "@typescript-eslint/parser", // Make ESLint compatible with TypeScript
+  parser: '@typescript-eslint/parser', // Make ESLint compatible with TypeScript
   parserOptions: {
     // Enable linting rules with type information from our tsconfig
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.eslint.json"],
+    project: ['./tsconfig.eslint.json'],
 
-    sourceType: "module", // Allow the use of imports / ES modules
+    sourceType: 'module', // Allow the use of imports / ES modules
 
     ecmaFeatures: {
       impliedStrict: true, // Enable global strict mode
@@ -23,38 +23,38 @@ module.exports = {
   },
 
   plugins: [
-    "@typescript-eslint", // Add some TypeScript specific rules, and disable rules covered by the typechecker
-    "import", // Add rules that help validate proper imports
-    "mocha", // Add rules for writing better Mocha tests
-    "prettier", // Allows running prettier as an ESLint rule, and reporting differences as individual linting issues
+    '@typescript-eslint', // Add some TypeScript specific rules, and disable rules covered by the typechecker
+    'import', // Add rules that help validate proper imports
+    'mocha', // Add rules for writing better Mocha tests
+    'prettier', // Allows running prettier as an ESLint rule, and reporting differences as individual linting issues
   ],
 
   extends: [
     // ESLint recommended rules
-    "eslint:recommended",
+    'eslint:recommended',
 
     // Add TypeScript-specific rules, and disable rules covered by typechecker
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
 
     // Add rules for import/export syntax
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
 
     // Add rules for Mocha-specific syntax
-    "plugin:mocha/recommended",
+    'plugin:mocha/recommended',
 
     // Add Airbnb + TypeScript support
-    "airbnb-base",
-    "airbnb-typescript/base",
+    'airbnb-base',
+    'airbnb-typescript/base',
 
     // Add rules that specifically require type information using our tsconfig
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
 
     // Enable Prettier for ESLint --fix, and disable rules that conflict with Prettier
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
 
   rules: {
