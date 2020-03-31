328d607711fa12b20c7aea750fe617c8020e7d54
Keefer Taylor <keefer.t@gmail.com>
2020-03-31 16:25:24 +0900

    remove legacy logic

--- test/XRP/signer-test.ts
+++ test/XRP/signer-test.ts
@@ -1,10 +1,7 @@
 import { assert } from 'chai'
 import * as rippleCodec from 'ripple-binary-codec'
 import FakeWallet from './fakes/fake-wallet'
-import { Payment as LegacyPayment } from '../../src/XRP/generated/legacy/payment_pb'
 import Signer from '../../src/XRP/signer'
-import { Transaction as LegacyTransaction } from '../../src/XRP/generated/legacy/transaction_pb'
-import { XRPAmount } from '../../src/XRP/generated/legacy/xrp_amount_pb'
 import 'mocha'
 import {
   CurrencyAmount,
@@ -27,49 +24,6 @@ import {
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 describe('signer', function(): void {
-  it('sign legacy transaction', function(): void {
-    // GIVEN an transaction and a wallet and expected signing artifacts.
-    const fakeSignature = 'DEADBEEF'
-    const wallet = new FakeWallet(fakeSignature)
-
-    const value = '1000'
-    const destination = 'XVPcpSm47b1CZkf5AkKM9a84dQHe3m4sBhsrA4XtnBECTAc'
-    const fee = '10'
-    const sequence = 1
-    const account = 'X7vjQVCddnQ7GCESYnYR3EdpzbcoAMbPw7s2xv8YQs94tv4'
-
-    const paymentAmount = new XRPAmount()
-    paymentAmount.setDrops(value)
-
-    const payment = new LegacyPayment()
-    payment.setDestination(destination)
-    payment.setXrpAmount(paymentAmount)
-
-    const transactionFee = new XRPAmount()
-    transactionFee.setDrops(fee)
-
-    const transaction = new LegacyTransaction()
-    transaction.setAccount(account)
-    transaction.setFee(transactionFee)
-    transaction.setSequence(sequence)
-    transaction.setPayment(payment)
-
-    // WHEN the transaction is signed with the wallet.
-    const signedTransaction = Signer.signLegacyTransaction(transaction, wallet)
-
-    // THEN the signing artifacts are as expected.
-    assert.exists(signedTransaction)
-
-    assert.isTrue(Utils.isHex(signedTransaction!.getTransactionSignatureHex()))
-
-    assert.equal(signedTransaction!.getTransactionSignatureHex(), fakeSignature)
-
-    assert.deepEqual(
-      signedTransaction!.getTransaction()!.toObject(),
-      transaction.toObject(),
-    )
-  })
-
   it('sign', function(): void {
     // GIVEN an transaction and a wallet and expected signing artifacts.
     const fakeSignature = 'DEADBEEF'
