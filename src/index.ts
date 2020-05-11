61e9ea9322e433dfbd464f6653b76e97f894e912
Keefer Taylor <keefer.t@gmail.com>
2020-05-11 21:26:19 +0900

    introduce wallet factory

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-export { Transaction } from './XRP/generated/org/xrpl/rpc/v1/transaction_pb'
-
-export { default as Wallet } from './XRP/wallet'
+// Exported Common Functionality.
 export { default as Utils } from './Common/utils'
-export { default as Signer } from './XRP/signer'
-export { default as Serializer } from './XRP/serializer'
-
-export { default as PayIDUtils } from './PayID/pay-id-utils'
-export { default as PayIDComponents } from './PayID/pay-id-components'
-
-// Fakes for Tests
-export { default as FakeWallet } from '../test/XRP/fakes/fake-wallet'
-
-// Type Exports
-export { WalletGenerationResult } from './XRP/wallet'
 export { ClassicAddress } from './Common/utils'
+
+// Exported PayID Functionality.
+export { default as PayIDComponents } from './PayID/pay-id-components'
+export { default as PayIDUtils } from './PayID/pay-id-utils'
+
+// Exported XRP Classes.
+export { default as FakeWallet } from '../test/XRP/fakes/fake-wallet'
+export { default as Serializer } from './XRP/serializer'
+export { default as Signer } from './XRP/signer'
+export { default as Wallet, WalletGenerationResult } from './XRP/wallet'
+export { default as WalletFactory } from './XRP/wallet-factory'
+export { Transaction } from './XRP/generated/org/xrpl/rpc/v1/transaction_pb'
