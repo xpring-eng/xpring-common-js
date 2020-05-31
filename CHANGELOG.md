3a5da181a9f5d33b531e96abe7c6719eec92a7c9
Keefer Taylor <keefer.t@gmail.com>
2020-05-31 16:20:44 +0900

    Introduce Wallet Factory (#267)
    * introduce wallet factory
    
    * run linter
    
    * update changelog
    
    * Remove unnamed import
    
    * Update and rename wallet-factory-test.ts to wallet-factory.test.ts
    
    * Update wallet-factory.test.ts
    
    * Update wallet-factory.test.ts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- A new class `WalletFactory` encapsulates functionality for creating a `Wallet` object from a public and private key.
+
 ## [5.0.4] - 2020-05-15
 
 This release contains minor deprecations of names of methods and classes to make this library more idiomatic with the wider JavaScript ecosystem.
---
+++ src/XRP/wallet-factory.ts
@@ -0,0 +1,30 @@
+import Wallet from './wallet'
+import Utils from '../Common/utils'
+
+/**
+ * Encapsulates various methods for generating Wallets.
+ */
+export default class WalletFactory {
+  /**
+   * Initialize a new WalletFactory.
+   *
+   * @param isTest Whether the wallet factory will generate wallets for a test network.
+   */
+  public constructor(public readonly isTest: boolean) {}
+
+  /**
+   * Generate a new wallet with the given keys.
+   *
+   * @param publicKey - A hexadecimal encoded public key.
+   * @param privateKey -  A hexadecimal encoded private key.
+   */
+  public walletFromKeys(
+    publicKey: string,
+    privateKey: string,
+  ): Wallet | undefined {
+    if (!Utils.isHex(publicKey) || !Utils.isHex(privateKey)) {
+      return undefined
+    }
+    return new Wallet(publicKey, privateKey, this.isTest)
+  }
+}
