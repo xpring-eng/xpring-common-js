4875c111eeb74185247830b5781d0691509566f3
Keefer Taylor <keefer.t@gmail.com>
2020-06-05 23:04:28 +0900

    remove duplicate exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 // Common Exports
 export { default as Utils } from './Common/utils'
 
-// XRP Exports
-export { Transaction } from './XRP/generated/org/xrpl/rpc/v1/transaction_pb'
-export { default as Serializer } from './XRP/serializer'
-export { default as Signer } from './XRP/signer'
-export { default as Wallet } from './XRP/wallet'
-export { default as XrpUtils, ClassicAddress } from './XRP/xrp-utils'
-
 // Pay ID Exports
 export { default as PayIdUtils, PayIDUtils } from './PayID/pay-id-utils'
 export {
@@ -20,5 +13,6 @@ export { default as FakeWallet } from '../test/XRP/fakes/fake-wallet'
 export { default as Serializer } from './XRP/serializer'
 export { default as Signer } from './XRP/signer'
 export { default as Wallet, WalletGenerationResult } from './XRP/wallet'
+export { default as XrpUtils, ClassicAddress } from './XRP/xrp-utils'
 export { default as WalletFactory } from './XRP/wallet-factory'
 export { Transaction } from './XRP/generated/org/xrpl/rpc/v1/transaction_pb'
