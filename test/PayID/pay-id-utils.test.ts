d872ed62fbab5e85a444bac315ef22af66e037e0
Amie <acorso@ripple.com>
2020-06-16 10:42:06 -0700

    Updates PayID parsing rules to support multiple "$" (#339)
    * update parsing rules for multi $
    
    * update too many dollar sign test
    
    * debug tests
    
    * get that codecov
    
    * no dollar sign test

--- test/PayID/pay-id-utils.test.ts
+++ test/PayID/pay-id-utils.test.ts
@@ -18,11 +18,36 @@ describe('PayIDUtils', function (): void {
     assert.equal(payIDComponents?.path, `/${path}`)
   })
 
-  it('parse Pay ID - multiple dollar signs', function (): void {
-    // GIVEN a Pay ID with too many '$'.
-    const host = 'xpring$money'
-    const path = 'georgewashington'
-    const rawPayID = `${host}$${path}`
+  it('parse Pay ID - valid multiple dollar signs', function (): void {
+    // GIVEN a Pay ID with more than one '$'.
+    const host = 'xpring.money'
+    const path = 'george$$$washington$'
+    const rawPayID = `${path}$${host}`
+
+    // WHEN it is parsed to components.
+    const payIDComponents = PayIdUtils.parsePayID(rawPayID)
+
+    // THEN the host and path are set correctly.
+    assert.equal(payIDComponents?.host, host)
+    assert.equal(payIDComponents?.path, `/${path}`)  
+  })
+
+  it('parse Pay ID - invalid multiple dollar signs (ends with $)', function (): void {
+    // GIVEN a Pay ID in which the host ends with a $.
+    const host = 'xpring.money$'
+    const path = 'george$$$washington$'
+    const rawPayID = `${path}$${host}`
+
+    // WHEN it is parsed to components.
+    const payIDComponents = PayIdUtils.parsePayID(rawPayID)
+
+    // THEN the Pay ID failed to parse.
+    assert.isUndefined(payIDComponents)
+  })
+
+  it('parse Pay ID - no dollar signs', function (): void {
+    // GIVEN a Pay ID that contains no dollar signs
+    const rawPayID = `georgewashington@xpring.money`
 
     // WHEN it is parsed to components.
     const payIDComponents = PayIdUtils.parsePayID(rawPayID)
@@ -35,7 +60,7 @@ describe('PayIDUtils', function (): void {
     // GIVEN a Pay ID with an empty host.
     const host = ''
     const path = 'georgewashington'
-    const rawPayID = `${host}$${path}`
+    const rawPayID = `${path}$${host}`
 
     // WHEN it is parsed to components.
     const payIDComponents = PayIdUtils.parsePayID(rawPayID)
@@ -45,10 +70,10 @@ describe('PayIDUtils', function (): void {
   })
 
   it('parse Pay ID - empty path', function (): void {
-    // GIVEN a Pay ID with an empty host.
+    // GIVEN a Pay ID with an empty path.
     const host = 'xpring.money'
     const path = ''
-    const rawPayID = `${host}$${path}`
+    const rawPayID = `${path}$${host}`
 
     // WHEN it is parsed to components.
     const payIDComponents = PayIdUtils.parsePayID(rawPayID)
