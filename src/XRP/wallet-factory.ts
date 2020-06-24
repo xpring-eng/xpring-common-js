c3bc332dfd99b7d20f63e6eee4fd92653bbec50b
Keefer Taylor <keefer.t@gmail.com>
2020-06-24 16:25:31 +0900

    fix typo

--- src/XRP/wallet-factory.ts
+++ src/XRP/wallet-factory.ts
@@ -56,9 +56,9 @@ export default class WalletFactory {
       
       // Add a 1 byte prefix to indicate that this is a secp256k1 key.
       // See: https://xrpl.org/cryptographic-keys.html#ed25519-key-derivation.
-      const prefixedPrivateKey = `00${privateKey}
+      const prefixedPrivateKey = `00${privateKey}`
       
-      return new Wallet(publicKey, prefixedPrivateKey`, this.isTest)
+      return new Wallet(publicKey, prefixedPrivateKey, this.isTest)
     } catch (error) {
       console.log(error)
       return undefined
