225a15b10f12d5dcedff84ff2ccb1721bf639a99
Keefer Taylor <keefer.t@gmail.com>
2020-09-01 15:30:52 -0700

    serialize signer quorum (#506)
    Co-authored-by: Keefer Taylor <keefer@keefertaylor.com>

--- src/XRP/serializer.ts
+++ src/XRP/serializer.ts
@@ -43,6 +43,7 @@ import {
   Condition,
   CancelAfter,
   FinishAfter,
+  SignerQuorum,
   RegularKey,
   SettleDelay,
   PaymentChannelSignature,
@@ -281,6 +282,7 @@ type OwnerJSON = string
 type ConditionJSON = string
 type CancelAfterJSON = number
 type FinishAfterJSON = number
+type SignerQuorumJSON = number
 type RegularKeyJSON = AccountAddressJSON
 type SettleDelayJSON = number
 type PaymentChannelSignatureJSON = string
@@ -1358,6 +1360,16 @@ const serializer = {
     return json
   },
 
+  /**
+   * Convert a SignerQuorum to a JSON representation.
+   *
+   * @param signerQuorum - The SignerQuorum to convert.
+   * @returns The SignerQuorum as JSON.
+   */
+  signerQuorumToJSON(signerQuorum: SignerQuorum): SignerQuorumJSON | undefined {
+    return signerQuorum.getValue()
+  },
+    
   /**
    * Convert an OfferCreate to a JSON representation.
    *
